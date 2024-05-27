b8c8c40cfb7b508b5dfe828d4ffdaa0df0c63ffd
FedericoCalio Caliò <federico.calio@studenti.malignani.ud.it>
2024-05-27 08:36:54 +0000

    Prove in classe

---
+++ PROGETTI_C++/progetto27/05.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string name;
+    cout<< "Benvenuto su GitPod" <<endl;
+    cout<< "Scrivi il tuo nome" <<endl;
+    cin>>name;
+    cout<< "Ciao " << name;
+
+    return 0;
+}
